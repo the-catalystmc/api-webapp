bb2b1df58a6feb63e2334319cf351f78b2185fc0
afcastaneda223 <af.castaneda223@gmail.com>
2021-08-03 17:28:08 -0300

    add function for pupulating meals

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,25 +1,26 @@
-import mealArray from './index.js'
-
 class APIUpdate{
+    getBeefBtn = document.getElementById('beefBtn');
+    getTemplate = document.querySelector('.mealTemplate');
+    container = document.querySelector('.cont');
+
     asyncRequest = async () => {
-        // const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=beef')
-        // const mealInfo = await response.json();
-        // return mealInfo
-        await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=beef')
-        // const meals = await response.json()
-        // return meals
-            .then(response => response.json())
-            .then(json => {
-                
-            }
-            //     {
-            //     json.meals.forEach(element => {
-            //         mealArray.mealList = element
-            //     });
-            // }
-            );
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=beef')
+        const mealInfo = await response.json();
+        return mealInfo
     }
 
+    asyncPopulate = async () => {
+        this.getBeefBtn.addEventListener('click',()=>{
+            this.asyncRequest().then((data) => {
+                data.meals.forEach(element => {
+                    const dish = this.getTemplate.content.firstElementChild.cloneNode(true);
+                    dish.querySelector('.card-img-top').src = element.strMealThumb;
+                    dish.querySelector('.card-title').innerText = element.strMeal;
+                    this.container.appendChild(dish);
+                });
+            })
+        })
+    }
 }
 
 const apiCall = new APIUpdate()
