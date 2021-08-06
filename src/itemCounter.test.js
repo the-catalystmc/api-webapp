0519db84074c31b0938efd99ff7529d5c7cf0729
afcastaneda223 <af.castaneda223@gmail.com>
2021-08-06 13:05:56 -0300

    fix unit test for itemcounter

--- src/itemCounter.test.js
+++ src/itemCounter.test.js
@@ -1,12 +1,12 @@
 /**
  * @jest-environment jsdom
  */
-
 import itemCount from './itemCounter';
 
-test('the array has 4 ', async () => {
-  const data = {
-    meals: [
+test('the array has 4 ',() => {
+  // Arrange 
+  const data = 
+    [
       {
         strMeal: 'Beef and Mustard Pie',
         strMealThumb: 'https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg',
@@ -27,27 +27,12 @@ test('the array has 4 ', async () => {
         strMealThumb: 'https://www.themealdb.com/images/media/meals/vtqxtu1511784197.jpg',
         idMeal: '52904',
       },
-    ],
-  };
-  const mealLenght = data.meals.lenght;
-  const itemCountTest = await itemCount.countMeals();
+    ];
 
-  expect(itemCountTest).toBe(mealLenght);
-});
+    // Act
+  const mealLength = data;
+  const itemCountTest = itemCount.countMeals(mealLength);
+  // Assert
+  expect(itemCountTest).toBe(4);
 
-test('if the array is empty will return 0 ', async () => {
-  const data = {
-    meals: [
-
-    ],
-  };
-  const mealLenght = data.meals.lenght;
-  const itemCountTest = await itemCount.countMeals();
-
-  expect(itemCountTest).toBe(mealLenght);
-});
-
-test('the array has elements it cant return 0 ', async () => {
-  const itemCountTest = await itemCount.countMeals();
-  expect(itemCountTest).not.toBe(0);
 });
